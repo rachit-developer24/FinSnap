78ad75fe4cdf2d922eb0dca0a6947fa16f66486e
rachit-developer24 <srachit841@gmail.com>
2026-04-01 02:23:54 +0100

    Initial Commit

---
+++ finSnap/finSnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  finSnapApp.swift
+//  finSnap
+//
+//  Created by Rachit Sharma on 01/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct finSnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
